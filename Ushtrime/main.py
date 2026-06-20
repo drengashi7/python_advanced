e3e1c4d34eb194c439b811534532eb7b96e1e27f
drengashi7 <167891457+drengashi7@users.noreply.github.com>
2026-06-20 12:50:45 +0200

    123

---
+++ Ushtrime/main.py
@@ -0,0 +1,50 @@
+#variabels
+
+level = 2 #int
+points = 2.3 #floats
+numri2 = "i dyt"
+
+print(level)
+
+level = 5
+print(numri2)
+
+startGame = False
+
+sartGame = True
+
+print(startGame)
+
+puntoret = ["Dreni","Marsi","Olti","vlera"]
+
+print(puntoret[3])
+
+puntoret.remove("Olti")
+
+puntoret.append("Donjeta")
+
+puntoret.insert(0, "hello"  )
+
+print(puntoret)
+
+levelGame = 2
+points=299
+
+
+levelGame = 300
+if(points==300):
+    levelGame=3
+    print("dil ni nivel tjeter")
+elif(points<300):
+    print("jeni ne nivelin 2")
+else:
+    print("anje kusht tuk u plotesua")
+
+print(levelGame)
+
+#metodat
+def pershendetje(name):
+    print("mirsevini",name)
+pershendetje("dreni")
+
+
